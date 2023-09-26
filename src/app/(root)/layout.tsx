4000e9441dd86a7b53e17538f98563b389bd788f
Ryan Yogan <ryanyogan@gmail.com>
2023-09-26 14:50:20 -0500

    feat // wip // right sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from "@/components/shared/left-sidebar";
 import Navbar from "@/components/shared/navbar/navbar";
+import RightSidebar from "@/components/shared/right-sidebar";
 
 export default function AuthLayout({
   children,
@@ -14,9 +15,10 @@ export default function AuthLayout({
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        Right Sidebar
+
+        <RightSidebar />
       </div>
-      Toaster
+      {/* Toaster */}
     </main>
   );
 }
