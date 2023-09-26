9e2f703a2c6ea5d8e30d60f1594762baa20577a8
Ryan Yogan <ryanyogan@gmail.com>
2023-09-26 16:55:51 -0500

    feat // sidebar complete

--- src/components/shared/render-tag.tsx
+++ src/components/shared/render-tag.tsx
@@ -18,7 +18,9 @@ export default function RenderTag({
       <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
         {name}
       </Badge>
-      {showCount && <p className="small-medium">{totalQuestions}</p>}
+      {showCount && (
+        <p className="small-medium text-dark500_light700">{totalQuestions}</p>
+      )}
     </Link>
   );
 }
